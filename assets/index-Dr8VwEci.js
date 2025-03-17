ba1695dc0ab28ee008664f20cd044cd1ca2d077e
ohgus <anakin1108@naver.com>
2025-03-17 17:22:55 +0900

    Updates

--- assets/index-Dr8VwEci.js
+++ assets/index-Dr8VwEci.js
@@ -231,7 +231,7 @@ const TextInput = (name, isRequired, helpText) => {
   return formItem;
 };
 const getStarIconSrc = (isFavorite) => {
-  return isFavorite ? "public/favorite-icon-filled.png" : "public/favorite-icon-lined.png";
+  return isFavorite ? "/favorite-icon-filled.png" : "favorite-icon-lined.png";
 };
 const StarIcon = (isFavorite) => {
   const iconSrc = getStarIconSrc(isFavorite);
